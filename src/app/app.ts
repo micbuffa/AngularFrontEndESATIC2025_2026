5838f306b568e72d2f29e74ce3e15ab2240a9d18
micbuffa <micbuffa@gmail.com>
2026-02-17 10:23:26 +0100

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('assignment-app');
+  titre = "Première application Angular !!!";
 }
